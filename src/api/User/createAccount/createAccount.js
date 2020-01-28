d50703046b2e69a520bd4e747260ee0c149a5e94
rednjzz <rednjzz@gmail.com>
2020-01-28 14:19:05 +0900

    createAccount 와 requestSecret에서 존재하는 사용자인지 검증

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,15 +1,21 @@
 import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
-    createAccount: async(_, args) => {
-      const { username, email, firstName ="", lastName = "", bio = "" } = args;
-      const exist = await prisma.$exists.user({email});
-      if (!exist) {
-        const user = await prisma.createUser({username, email, firstName, lastName, bio})
-        return user;
-      } else {
-        throw Error("this user is exist");
+    createAccount: async (_, args) => {
+      const { username, email, firstName ="", lastName = "", bio = "" } = args;  
+      try{
+        const exist = await prisma.$exists.user({OR: [{username}, {email}]});
+        if (!exist) {
+          const user = await prisma.createUser({username, email, firstName, lastName, bio})
+          return true;
+        } else {
+          return false;
+        }
+      } catch (e) {
+        throw Error(e);
       }
+
+      
       
     }
   }
--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -5,15 +5,22 @@ export default {
   Mutation: {
     requestSecret: async (_, args) => {
       const { email } = args;
-      const loginSecret = generateSecret();
-      try {
-        await sendSecretMail(email, loginSecret);
-        await prisma.updateUser({data:{loginSecret}, where: { email }});
-        return true;
-      } catch (e) {
-        console.log(e);
+      const exist = await prisma.$exists.user({email});
+      if (exist) {
+        const loginSecret = generateSecret();
+        try {
+          await sendSecretMail(email, loginSecret);
+          await prisma.updateUser({data:{loginSecret}, where: { email }});
+          return true;
+        } catch (e) {
+          console.log(e);
+          return false;
+        }
+      } else {
+        throw Error("Unregistered Email, Please Sign up")
         return false;
       }
+      
     }
   }
 };
