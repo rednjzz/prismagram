cca373083d5f5e4af94eda4bd7f79a9b2eece6a8
rednjzz <rednjzz@gmail.com>
2020-02-04 13:33:46 +0900

    add phoneNumber in the mutations

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -2,11 +2,11 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     createAccount: async (_, args) => {
-      const { username, email, firstName ="", lastName = "", bio = "" } = args;  
+      const { username, email, phoneNumber, firstName ="", lastName = "", bio = "" } = args;  
       try{
-        const exist = await prisma.$exists.user({OR: [{username}, {email}]});
+        const exist = await prisma.$exists.user({OR: [{username}, {email}, {phoneNumber}]});
         if (!exist) {
-          const user = await prisma.createUser({username, email, firstName, lastName, bio})
+          const user = await prisma.createUser({username, email, phoneNumber, firstName, lastName, bio})
           return true;
         } else {
           return false;
--- src/api/User/editUser/editUser.js
+++ src/api/User/editUser/editUser.js
@@ -5,11 +5,12 @@ export default {
     editUser: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      const {email, avatar, firstName, lastName, bio} = args;
+      const {email, phoneNumber, avatar, firstName, lastName, bio} = args;
       return prisma.updateUser({
         where: {id: user.id},
         data:{
           email,
+          phoneNumber,
           avatar,
           firstName,
           lastName,
